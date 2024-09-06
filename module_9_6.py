e5dbe3c8dadddcc7062d13a50842354d6aa2dbd9
AnastasiyaV91 <anastasiyakiseleva@bk.ru>
2024-09-07 00:31:30 +0300

    module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -1,9 +1,7 @@
-def all_variants(text):
-    for k in text:           # Цикл по каждому элементу строки text
-        yield k              # Возвращаем каждый элемент text
-    yield text[0] + text[1]  # Возвращаем первый и второй элемент text
-    yield text[1] + text[2]  # Возвращаем второй и третий элемент text
-    yield text               # Возвращаем исходную строку text
+def all_variants(text):                 # Создаем функцию-генератор
+    for i in range(len(text)):          # Внешний цикл, который итерируется по длине строки text
+        for j in range(len(text) - i):  # Внутрений цикл, который итерируется от 0 до len(text) - i
+         yield text[j: i + j + 1]       # Возвращаем подстроки начиная с индекса j до индекса i + j + 1
 
 a = all_variants("abc")
 for i in a:
